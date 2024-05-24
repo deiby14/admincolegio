c268552555f87b7d5b7151c39a58b3adf192fd52
unknown <deibycesar10@gmail.com>
2024-05-24 19:12:19 +0200

    trabajo final

---
+++ formulario/formeditaralum.php
@@ -0,0 +1,11 @@
+<form action="" method="post">
+    <label>Nombre:</label>
+    <input type="text" name="Nombre"><br>
+    <label>Apellido:</label>
+    <input type="text" name="Apellido"><br>
+    <label>Contacto:</label>
+    <input type="number" name="Contacto"><br>
+    <label>Curso:</label>
+    <input type="text" name="Curso"><br>
+    <input type="submit">
+</form>
